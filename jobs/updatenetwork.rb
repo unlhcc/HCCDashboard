1e967d5bc1889335612223ff2bb848d9ff435b83
Caughlin Bohn <computerwhisperer21@gmail.com>
2019-05-08 09:08:32 -0500

    Numbers must be large enough to generate monetary value

--- jobs/updatenetwork.rb
+++ jobs/updatenetwork.rb
@@ -5,6 +5,6 @@ end
 (1..30).each do |i|
     send_event('ShorNetworkGraph', { points: points}  )
     send_event('PkiNetworkGraph', { points: points}  )
-    send_event('HCCAmazonPrice', { network_bandwidth: 10 })
+    send_event('HCCAmazonPrice', { network_bandwidth: 10000000 })
     sleep(0.2)
 end
